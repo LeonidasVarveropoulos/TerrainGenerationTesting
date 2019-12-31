f0e40e69adfa8544b0383da2eaedc4428da42b83
LeonidasVarveropoulos <leonidas@vrhome.net>
2019-12-31 12:19:17 -0600

    changed height

--- WorldGeneration/Assets/Scripts/MapGenerator.cs
+++ WorldGeneration/Assets/Scripts/MapGenerator.cs
@@ -19,6 +19,7 @@ public class MapGenerator : MonoBehaviour
     public Vector2 offset;
 
     public float meshHeightMultiplier;
+    public AnimationCurve meshHeightCurve;
 
     public bool autoUpdate;
 
@@ -56,7 +57,7 @@ public class MapGenerator : MonoBehaviour
         }
         else if (drawMode == DrawMode.Mesh)
         {
-            display.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap, meshHeightMultiplier), TextureGenerator.TextureFromColorMap(colorMap, mapWidth, mapHeight));
+            display.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap, meshHeightMultiplier, meshHeightCurve), TextureGenerator.TextureFromColorMap(colorMap, mapWidth, mapHeight));
         }
     }
     private void OnValidate()
